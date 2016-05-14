c5be9e97424662f2156b1bc662da5099ad3b1706
Kaspar Tilk <kaspar.tilk@itcollege.ee>
2016-05-14 12:43:05 +0300

    notificationiga jauramine pooleli(andmete saatmine notificationi ja rakenduse vahel tegemata), muu enam vähem timmis

--- app/src/main/java/com/akaver/gmapdemo02/MainActivity.java
+++ app/src/main/java/com/akaver/gmapdemo02/MainActivity.java
@@ -1,7 +1,12 @@
 package com.akaver.gmapdemo02;
 
 import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Criteria;
@@ -12,6 +17,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +27,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +44,7 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
@@ -50,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private String provider;
 
+    private NotificationManager mNotificationManager;
+    private BroadcastReceiver mBroadcastReceiver;
+
     private int markerCount = 0;
     private Location locationPrevious;
     private Location locationStart;
@@ -62,6 +73,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private double cResetDistance;
     private double cResetDistanceLine;
     private double speedCurrent;
+    private long timePrevious;
 
     private Polyline mPolyline;
     private PolylineOptions mPolylineOptions;
@@ -84,6 +96,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //textViewBroadcast.setText(intent.toString()+" "+(new Date()).toString());
+
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("notification-broadcast-addwaypoint");
+        intentFilter.addAction("notification-broadcast-resettripmeter");
+        registerReceiver(mBroadcastReceiver, intentFilter);
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -123,6 +150,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         textViewCResetDistance = (TextView) findViewById(R.id.textview_creset_distance);
         textViewCresetDistanceLine = (TextView) findViewById(R.id.textview_creset_line);
         textViewSpeed = (TextView) findViewById(R.id.textview_speed);
+
+        createNotification();
     }
 
     private void updateDisplay() {
@@ -138,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         textViewCresetDistanceLine.setText(df.format(cResetDistanceLine));
 
         textViewSpeed.setText(df.format(speedCurrent));
+
     }
 
     private void updateDistances(Location currentLocation){
@@ -147,6 +177,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         totalDistance += distance;
 
         totalDistanceLine = locationStart.distanceTo(currentLocation);
+
 
         // distance from last waypoint
         if (markerCount != 0){
@@ -337,7 +368,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void buttonCResetClicked(View view){
+        resetTracker();
+    }
+
+    public void resetTracker(){
         locationLastCReset = locationPrevious;
+        mGoogleMap.clear();
+        markerCount = 0;
         cResetDistance = 0;
     }
 
@@ -367,6 +404,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onLocationChanged(Location location) {
+        long timeCurrent = System.currentTimeMillis() / 1000L;
         LatLng newPoint = new LatLng(location.getLatitude(), location.getLongitude());
 
         if (mGoogleMap==null) return;
@@ -393,14 +431,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             mPolylineLine.setPoints(points);
         }
 
+        if (locationStart == null){
+            locationStart = location;
+            return;
+        }
         if (locationPrevious != null){
+            speedCurrent = calculateSpeed(timePrevious, timeCurrent, locationPrevious, location);
             updateDistances(location);
         }
-        speedCurrent = location.getSpeed();
+
         locationPrevious = location;
         updateDisplay();
+        updateNotification();
+        timePrevious = timeCurrent;
+    }
+    public double calculateSpeed(long prevTime, long curTime, Location prevLoc, Location curLoc){
+        long time = curTime - prevTime;
+        double dist = prevLoc.distanceTo(curLoc);
+        return dist/time;
     }
 
+/*    public String convertSpeedToMinKM(double speed){
+        double minKM = (50/3)/speed;
+        double minutes = (minKM % 3600)/60;
+        double seconds = minKM % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }*/
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -450,4 +506,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    public void createNotification(){
+
+        // get the view layout
+        RemoteViews remoteView = new RemoteViews(
+                getPackageName(), R.layout.notification);
+
+        // define intents
+        PendingIntent pIntentAddWaypoint = PendingIntent.getBroadcast(
+                this,
+                0,
+                new Intent("notification-broadcast-addwaypoint"),
+                0
+        );
+
+        PendingIntent pIntentResetTripmeter = PendingIntent.getBroadcast(
+                this,
+                0,
+                new Intent("notification-broadcast-resettripmeter"),
+                0
+        );
+
+        // bring back already running activity
+        // in manifest set android:launchMode="singleTop"
+        PendingIntent pIntentOpenActivity = PendingIntent.getActivity(
+                this,
+                0,
+                new Intent(this, MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        // attach events
+        remoteView.setOnClickPendingIntent(R.id.buttonAddWayPointNotif, pIntentAddWaypoint);
+        remoteView.setOnClickPendingIntent(R.id.buttonResetTripmeterNotif, pIntentResetTripmeter);
+        remoteView.setOnClickPendingIntent(R.id.buttonOpenActivityNotif, pIntentOpenActivity);
+
+        // build notification
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContent(remoteView)
+                        .setSmallIcon(R.drawable.ic_my_location_white_48dp);
+
+        // notify
+        mNotificationManager.notify(4, mBuilder.build());
+
+    }
+
+    public void updateNotification(){
+        RemoteViews remoteView = new RemoteViews(
+                getPackageName(), R.layout.notification);
+        DecimalFormat df = new DecimalFormat("#");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        textViewTotalDistance.setText(df.format(totalDistance));
+        remoteView.setTextViewText(R.id.textViewTripmeterMetricsNotif, df.format(cResetDistance));
+        remoteView.setTextViewText(R.id.textViewWayPointMetricsNotif, "" + markerCount);
+    }
+
+    @Override
+    public void onDestroy(){
+        unregisterReceiver(mBroadcastReceiver);
+        super.onDestroy();
+    }
 }
